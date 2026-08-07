b47198de7dba3a5a1ec679e08d0aeec049a2ae7e
aagilmore <aagilmore@me.com>
2026-08-07 23:14:44 +0000

    Commit 0031 Create Atlas web dashboard HTML skeloton

--- web_app.py
+++ web_app.py
@@ -6,9 +6,59 @@ app = Flask(__name__)
 @app.route("/")
 def dashboard():
     return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Atlas Dashboard</title>
+    </head>
+
+    <body>
+
     <h1>ATLAS</h1>
     <h2>Weekly Dashboard</h2>
-    <p>If you can read this, Atlas is running in a browser.</p>
+
+    <hr>
+
+    <h2>Strategic Scorecard</h2>
+
+    <div id="strategic-scorecard">
+        Strategic Scorecard goes here
+    </div>
+
+    <hr>
+
+    <h2>Weekly Execution Scorecard</h2>
+
+    <div id="execution-scorecard">
+        Weekly Execution goes here
+    </div>
+
+    <hr>
+
+    <h2>Atlas Weekly Brief</h2>
+
+    <div id="weekly-brief">
+        Weekly Brief goes here
+    </div>
+
+    <hr>
+
+    <h2>Pillar Status</h2>
+
+    <div id="pillar-status">
+        Pillar cards go here
+    </div>
+
+    <hr>
+
+    <h2>Upcoming Events</h2>
+
+    <div id="events">
+        Events go here
+    </div>
+
+    </body>
+    </html> 
     """
 
 
